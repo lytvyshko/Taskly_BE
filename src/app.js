2646ccf3e047adfa8aeba4e3369a8832b57e7715
Pavlo Lytvyshko <pavlo.lytvyshko@jointoit.com>
2026-07-15 18:41:51 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import express from 'express';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log(`Server running on port ${PORT}`));
+
+
