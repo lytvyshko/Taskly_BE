deea2f7d67cd9f53c1f35550d918bccc103c0aeb
Pavlo Lytvyshko <pavlo.lytvyshko@jointoit.com>
2026-08-17 21:02:45 +0300

    update sameSite option

--- src/config/cookies.js
+++ src/config/cookies.js
@@ -1,6 +1,6 @@
 export const refreshCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: 'lax',
+  sameSite: 'none',
   maxAge: 30 * 24 * 60 * 60 * 1000,
 };
